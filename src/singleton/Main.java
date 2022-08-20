47489ac3dfaed5b3246bf88080198d4a15ef8dcf
nodwon <nodwon5024@gmail.com>
2022-08-20 15:20:47 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/encodings.xml
    #	src/singleton/Main.java

--- src/singleton/Main.java
+++ src/singleton/Main.java
@@ -1,9 +1,14 @@
 package singleton;
 
+import Proxy.IBroswser;
 import adapter.*;
+import aop.AopBrowser;
 import singleton.Aclass;
 import singleton.Bclass;
 import singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
     public static void main(String[] args) {
         Aclass aClazz = new Aclass();
@@ -22,6 +27,22 @@ public class Main {
         AairConditioner aairConditioner = new AairConditioner();
         Electrionic110V airAdapter = new SocketAdapter(aairConditioner);
         connect(airAdapter);
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBroswser aopbroswser = new AopBrowser("www.naver.com",
+                () ->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+
+                },() ->{
+                long now  = System.currentTimeMillis();
+                end.set(now -start.get());
+
+        });
+        aopbroswser.show();
+        System.out.println("loding time:" +end.get());
     }
     //콘센트
     public static void connect(Electrionic110V electrionic110V){
